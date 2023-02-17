6f8e037b49d3b08470357d59361d26470a063ded
Moreno Giussani <giussani.moreno@gmail.com>
2023-02-17 13:40:27 +0100

    Initial work to integrate dht producer

--- install_libVC.sh
+++ install_libVC.sh
@@ -9,6 +9,31 @@ cd build &&
 cmake -DCMAKE_INSTALL_PREFIX="." .. &&
 make -j4 &&
 make install &&
+cd ../../ &&
+# Install libVC-dht-prod
+rm -rf libVersioningCompilerProd &&
+git clone https://github.com/Focshole/libvc-dht-producer libVersioningCompilerProd &&
+cd libVersioningCompilerProd &&
+ln -s ../libVC libVC  &&
+mkdir build &&
+cd build &&
+cmake -DCMAKE_INSTALL_PREFIX="." .. &&
+make -j4 &&
+make install &&
+cd ../../ &&
+# Install libVC-dht-cons
+rm -rf libVersioningCompilerCons &&
+ln -s libVC ../libVC &&
+git clone https://github.com/Focshole/libvc-dht-consumer libVersioningCompilerCons &&
+cd libVersioningCompilerCons &&
+ln -s ../libVC libVC  &&
+mkdir build &&
+cd build &&
+cmake -DCMAKE_INSTALL_PREFIX="." .. &&
+make -j4 &&
+make install &&
+cd ../../ &&
+# Copy cmake files
 mkdir -p ../../cmake &&
 cp lib/cmake/FindLibVersioningCompiler.cmake ../../cmake/FindLibVersioningCompiler.cmake &&
 cd ../../
