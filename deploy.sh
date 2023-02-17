e27abcf4ed2f0d37524b688556728dacb3ad288e
Moreno Giussani <giussani.moreno@gmail.com>
2023-02-17 14:45:57 +0100

    Refactoring install & deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
-rm -rf libVC/build/
-rm -rf build/
-rm -rf cmake/
+rm -rf libVC/build/ &&
+rm -rf libVersioningCompilerProd/build/ &&
+rm -rf libVersioningCompilerCons/build/ &&
+rm -rf build/ &&
+rm -rf cmake/ &&
 
-mkdir libVC/build
-mkdir build
-mkdir cmake
+mkdir libVC/build &&
+mkdir libVersioningCompilerProd/build &&
+mkdir libVersioningCompilerCons/build &&
+mkdir build &&
+mkdir cmake &&
 
-cd libVC/build
-cmake .. -DCMAKE_INSTALL_PREFIX="."
-make && make install || { echo "Failed to build libVC" && exit 2; }
+cd libVC/build &&
+cmake .. -DCMAKE_INSTALL_PREFIX="." &&
+make && make install || { echo "Failed to build libVC" && exit 2; } &&
+cd ../.. &&
 
-cd ../..
-cp libVC/config/FindLibVersioningCompiler.cmake cmake/
+cd libVersioningCompilerProd/build &&
+cmake .. -DCMAKE_INSTALL_PREFIX="." &&
+make && make install || { echo "Failed to build libVCProd" && exit 2; } &&
+cd ../.. &&
 
-cd build/
-cmake .. -DCMAKE_INSTALL_PREFIX="."
+cd libVersioningCompilerCons/build &&
+cmake .. -DCMAKE_INSTALL_PREFIX="." &&
+make && make install || { echo "Failed to build libVCCons" && exit 2; } &&
+cd ../.. &&
+
+cp libVC/build/lib/cmake/FindLibVersioningCompiler.cmake cmake/ &&
+cp libVersioningCompilerProd/build/lib/cmake/FindLibVersioningCompilerProd.cmake cmake/
+cp libVersioningCompilerProd/build/lib/cmake/FindLibVersioningCompilerCons.cmake cmake/
+cp libVersioningCompilerProd/cmake/Findlibzmq.cmake cmake/ &&
+
+cd build/ &&
+cmake .. -DCMAKE_INSTALL_PREFIX="." &&
 make && make install || { echo "Failed to build program" && exit 2; }
--- install_libVC.sh
+++ install_libVC.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-
+# Clone libVC
 rm -rf libVC
 git clone https://github.com/Focshole/libVersioningCompiler.git libVC &&
 cd libVC &&
 git checkout dev &&
+# Build libVC
 mkdir build &&
 cd build &&
 cmake -DCMAKE_INSTALL_PREFIX="." .. &&
 make -j4 &&
 make install &&
+cp lib/cmake/FindLibVersioningCompiler.cmake ../../cmake/FindLibVersioningCompiler.cmake &&
 cd ../../ &&
 # Install libVC-dht-prod
 rm -rf libVersioningCompilerProd &&
 git clone https://github.com/Focshole/libvc-dht-producer libVersioningCompilerProd &&
 cd libVersioningCompilerProd &&
+# add libVC
 ln -s ../libVC libVC  &&
+# add libVC cmake file
+cp ./libVC/build/lib/cmake/FindLibVersioningCompiler.cmake ./cmake/ &&
+# Build libVC-dht-prod
 mkdir build &&
 cd build &&
 cmake -DCMAKE_INSTALL_PREFIX="." .. &&
@@ -23,10 +29,13 @@ make install &&
 cd ../../ &&
 # Install libVC-dht-cons
 rm -rf libVersioningCompilerCons &&
-ln -s libVC ../libVC &&
 git clone https://github.com/Focshole/libvc-dht-consumer libVersioningCompilerCons &&
 cd libVersioningCompilerCons &&
+# add libVC
 ln -s ../libVC libVC  &&
+# add libVC cmake file
+cp ./libVC/build/lib/cmake/FindLibVersioningCompiler.cmake ./cmake/ &&
+# Build libVC-dht-cons
 mkdir build &&
 cd build &&
 cmake -DCMAKE_INSTALL_PREFIX="." .. &&
@@ -34,8 +43,9 @@ make -j4 &&
 make install &&
 cd ../../ &&
 # Copy cmake files
-mkdir -p ../../cmake &&
-cp lib/cmake/FindLibVersioningCompiler.cmake ../../cmake/FindLibVersioningCompiler.cmake &&
-cd ../../
-
+mkdir -p cmake &&
+cp libVC/build/lib/cmake/FindLibVersioningCompiler.cmake ./cmake/ &&
+cp libVersioningCompilerProd/build/lib/cmake/FindLibVersioningCompilerProd.cmake cmake/ &&
+cp libVersioningCompilerProd/cmake/Findlibzmq.cmake cmake/ &&
+cp libVersioningCompilerProd/build/lib/cmake/FindLibVersioningCompilerCons.cmake cmake/
 
